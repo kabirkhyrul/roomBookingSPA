2ae4efe19c74866f9a953b81e24c0c86e5843363
khyrul kabir <khyrulkabir01@gmail.com>
2019-11-19 17:07:09 -0600

    fixed robots.txt error

--- public/javascripts/db.js
+++ public/javascripts/db.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'njspa'
+    user: 'vxpgn3sb_kabir',
+    password: 'kabir29',
+    database: 'vxpgn3sb_hotelBook'
 });
 connection.connect(function (error) {
     if (!!error) {
